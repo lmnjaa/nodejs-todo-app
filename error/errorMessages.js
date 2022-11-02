e92f177c8ece68954dc81721838dc08552c870f7
lmnjaa <nemanjatomanicn@gmail.com>
2022-11-02 10:22:25 +0100

    [CHANGE] Error message and installed jest npm package

--- error/errorMessages.js
+++ error/errorMessages.js
@@ -1,6 +1,6 @@
 const NoContent = "No content found.";
 const UnAuthorized = "You are not authorized to see this content.";
-const Error404 = "Error404. Please, try again later";
+const Error404 = "Error404. Page or content not fround.";
 const Error500 = "Error500. Something happend. Please, try again later.";
 
 module.exports = {
